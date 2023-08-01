a1e45c56a5a60d3ef58b2cd4cf231136c21bbef3
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2023-08-01 14:33:34 +0100

    Work in progress

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,34 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import svgLoader from "vite-svg-loader";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    viteSingleFile(),
+    svgLoader({
+      defaultImport: "raw",
+    }),
+  ],
 });
+
+// import { defineConfig } from "vite";
+// // import { svelte } from "@sveltejs/vite-plugin-svelte";
+// import { viteSingleFile } from "vite-plugin-singlefile";
+// import svgLoader from "vite-svg-loader";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     svelteKit(),
+//     viteSingleFile(),
+//     svgLoader({
+//       defaultImport: "raw", // or 'raw'
+//     }),
+//   ],
+//   build: {
+//     watch: true,
+//   },
+// });
